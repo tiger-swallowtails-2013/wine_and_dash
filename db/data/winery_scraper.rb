aab08030632fa58c9cb4a1b0be9a89da5cd907ae
Apprentice <apprentice@dbc18.local>
2013-09-30 18:14:55 -0700

    got winery CSV, working out how to import to db

---
+++ db/data/winery_scraper.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+
+
+doc = Nokogiri::HTML(open('http://www.napavintners.com/wineries/wineries_tastings.asp')) do |config|
+  config.strict.nonet
+end
+
+
+
+
+winery_data = doc.css('h4')
+
+CSV.open("wineries.csv", "w") do |csv|
+  winery_data.each_with_index do |winery,index|
+    #NAMES
+    p winery_name = winery.children[1].children.text
+    #URL
+    href = winery.children[1].attributes['href'].text
+    p url =  "http://www.napavintners.com" + href
+    #ADDRESS
+    p address = doc.css('.span8')[index].text.split("\n")[3].strip
+
+    csv << [winery_name, url, address]
+    break if winery_name == 'ZD Wines'
+  end
+end
+# p hrefs.inspect
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930182157) do
+ActiveRecord::Schema.define(version: 20130930225301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "password"
-    t.string "username"
-    t.string "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password"
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "wineries", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
